1415fbf1e1c4c9042c515273063c40bf0228e902
Aimable <aimablerubana@gmail.com>
2023-07-14 03:39:38 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main . A program that prints a line with puts functio
+n
+ * Return: 0(success)
+ */
 int main() {
     puts("Programming is like building a multilingual puzzle");
     return 0;
