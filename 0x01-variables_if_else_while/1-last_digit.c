15bfebbf9f358dc4f0d5b1959e07206500411ac0
Aimable <aimablerubana@gmail.com>
2023-07-14 21:27:58 -0700

    Task 1. The last digit;

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry points
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+int n; n = 25
+m = 25 / 10 = 2 % 5
+int m; m = 5
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+n = n % 10;
+if (n > 5)
+printf("Last digit of %d is %d and is greater than 5\%", n, m1;
+if (m == 0)
+printf("last digit of %d is %d and is 0\n", n, m);
+if (m < 6 && m != 0)
+printf("Last digit of %d is % and is less than 6 and not 0\n", n, m);
+return (0);
+}
