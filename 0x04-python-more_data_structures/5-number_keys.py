e897cf992cbcf6108dfe9987cc268a6348ba9b1c
Aimable-Ngoga <aimablerubana@gmail.com>
2023-12-08 09:08:13 -0800

    Another changes

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-only_diff_elements = __import__('4-only_diff_elements').only_diff_elements
+def number_keys(a_dictionary):
+    num = 0
+    list_keys = list(a_dictionary.keys())
 
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-od_set = only_diff_elements(set_1, set_2)
-print(sorted(list(od_set)))
+    for i in list_keys:
+        num += 1
+
+    return (num)
